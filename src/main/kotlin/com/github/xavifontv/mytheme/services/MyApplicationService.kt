f314d13c59c7f0470522f159a2ee01802fccc66e
GitHub Action <action@github.com>
2022-06-23 19:43:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/xavifontv/mytheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xavifontv/mytheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xavifontv.mytheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xavifontv.mytheme.MyBundle
 
 class MyApplicationService {
 
